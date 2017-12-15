8ae84bf67be73faeea774092ada19dda7fa9316e
Ariel Tubaltsev <tubaltzev@gmail.com>
2017-12-15 05:31:32 -0800

    add locales

--- src/popup.js
+++ src/popup.js
@@ -4,22 +4,19 @@ chrome.runtime.sendMessage(
       var section = document.querySelector('body>div>div>div>section');
       var results = response.result;
       var ol = document.createElement('ol');
-      var li, p, em, code, text;
+      var li, p, code, text;
       var i;
       for (i = 0; i < results.length; i++ ) {
         li = document.createElement('li');
         p = document.createElement('p');
-        em = document.createElement('em');
-        //em.textContent = i + 1;
         code = document.createElement('code');
         code.textContent = results[i].url;
         text = document.createTextNode(
           chrome.i18n.getMessage('navigationDescription',
             [results[i].numRequests,
             results[i].average]));
-        //p.appendChild(em);
         p.appendChild(code);
-        //p.appendChild(text);
+        p.appendChild(text);
         li.appendChild(p);
         ol.appendChild(li);
       }
