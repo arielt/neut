6b0ca0df8d68aa7044f0c23ec483ef3632eb280f
Ariel Tubaltsev <tubaltzev@gmail.com>
2017-12-17 23:20:26 -0800

    jslint popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,21 @@
+"use strict";
+
+/*jslint browser:true */
+/*global chrome, $, jQuery, alert*/
+
 // console log will be seen in dev console of the popup
 
 chrome.runtime.sendMessage(
     {'type': 'getMostRequestedUrls'},
     function generateList(response) {
-      var results = response.result;
-      for (var i=0; i < results.length; i++) {
-        $('#hosts-table').find('tbody').append(
-          "<tr><td>" +
-          results[i].url + "</td><td class='center'>" +
-          results[i].numRequests + "</td><td class='center'>" +
-          Math.round(results[i].average) + "</td></tr>"
-        );
-
-      }
-    });
+        var i, results = response.result;
+        for (i = 0; i < results.length; i += 1) {
+            $('#hosts-table').find('tbody').append(
+                "<tr><td>" +
+                    results[i].url + "</td><td class='center'>" +
+                    results[i].numRequests + "</td><td class='center'>" +
+                    Math.round(results[i].average) + "</td></tr>"
+            );
+        }
+    }
+);
