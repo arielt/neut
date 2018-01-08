f09488272ae29dbf60b8088c24ed2b24ad6a6c3f
Ariel Tubaltsev <tubaltzev@gmail.com>
2018-01-07 22:47:47 -0800

    report top sites to popup
    store hosts in memory map
    
    save and load hosts from the storage a whole
    
    adjust popup html and js

--- src/event_page.js
+++ src/event_page.js
@@ -12,6 +12,62 @@
 /*global chrome*/
 
 var CACHE_HIT_TIME = 30;
+var TOP_SITES = 10;
+
+// in-memory map of hosts
+var hosts = {};
+
+function loadStorageData() {
+    chrome.storage.local.get("hosts", function (result) {
+        if (result.hosts) {
+            hosts = result.hosts;
+        }
+    });
+}
+
+function saveStorageData() {
+    chrome.storage.local.set("hosts", hosts);
+}
+
+function updateHosts(msg, duration) {
+    var host = hosts[msg.hostname];
+
+    if (host === undefined) {
+        hosts[msg.hostname] = {
+            'reqNum': 1,
+            'avg': duration,
+            'last': duration
+        };
+    } else {
+        host.reqNum += 1;
+        host.avg = host.avg + (duration - host.avg) / host.reqNum;
+        host.last = duration;
+    }
+
+    saveStorageData();
+}
+
+function topSites() {
+    var arr = [], k, host;
+
+    for (k in hosts) {
+        if (hosts.hasOwnProperty(k)) {
+            host = hosts[k];
+            arr.push({
+                host: k,
+                reqNum: host.reqNum,
+                avg: host.avg,
+                last: host.last
+            });
+        }
+    }
+
+    arr.sort(function (a, b) {
+        return b.reqNum - a.reqNum;
+    });
+
+    return arr.slice(0, TOP_SITES);
+}
 
 /*jslint unparam: true*/
 chrome.runtime.onMessage.addListener(
@@ -21,30 +77,18 @@ chrome.runtime.onMessage.addListener(
             var duration = msg.timing.responseEnd - msg.timing.connectStart;
             chrome.browserAction.setBadgeText({text: String((duration / 1000).toFixed(2)), tabId: sender.tab.id});
             if (duration > CACHE_HIT_TIME) {
-                chrome.storage.local.get("hosts", function (result) {
-                    if (!result.hosts) {
-                        result.hosts = {};
-                    }
-
-                    var host = result.hosts[msg.hostname];
-                    if (host === undefined) {
-                        result.hosts[msg.hostname] = {
-                            'reqNum': 1,
-                            'avg': duration,
-                            'last': duration
-                        };
-                    } else {
-                        host.reqNum += 1;
-                        host.avg = host.avg + (duration - host.avg) / host.reqNum;
-                        host.last = duration;
-                    }
-                    chrome.storage.local.set(result);
-                });
+                updateHosts(msg, duration);
             }
             break;
         case 'topSites':
+            sendResponse({
+                result: topSites()
+            });
             break;
         }
     }
 );
 /*jslint unparam: false*/
+
+// start event page
+loadStorageData();
