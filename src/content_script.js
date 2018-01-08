e30041376819fc50fbdadce47dd064640df5f555
Ariel Tubaltsev <tubaltzev@gmail.com>
2018-01-07 17:39:19 -0800

    collect performance data
    check document is loaded, otherwise add event listenr
    
    yeild when collecting metrics

--- src/content_script.js
+++ src/content_script.js
@@ -1,19 +1,46 @@
 /**
  * Content script to be injected to the loaded page. Runs with default run_at
- * parameter.
- * https://developer.chrome.com/extensions/content_scripts
+ * parameter: https://developer.chrome.com/extensions/content_scripts
+ * Performace timing: https://www.w3.org/TR/navigation-timing/
  */
 
 "use strict";
-/*jslint browser:true */
+/*jslint browser:true, todo: true*/
+/*global performance*/
 
 function perfTime() {
-    console.log("perfTime called");
+    // yield, otherwise loadEventEnd will be zero
+    setTimeout(function () {
+        var perf = performance.timing, timing;
+
+        if (perf.loadEventEnd <= 0) {
+            return;
+        }
+
+        // TODO: exclude metrics not used in immediate calculation
+        timing = {
+            'fetchStart': perf.fetchStart,
+            'domainLookupStart': perf.domainLookupStart,
+            'domainLookupEnd': perf.domainLookupStart,
+            'connectStart': perf.connectStart,
+            'connectEnd': perf.connectEnd,
+            'requestStart': perf.requestStart,
+            'responseStart': perf.responseStart,
+            'responseEnd': perf.responseEnd,
+            'string': JSON.stringify(perf)
+        };
+
+        // send message instead
+        console.log(timing);
+
+    }, 0);
 }
 
-// with default run_at, browser chooses when to inject the script
-if (document.readyState === "complete") {
-    perfTime();
-} else {
-    window.addEventListener("load", perfTime);
+if (window.performance && performance.timing) {
+    // with default run_at, browser chooses when to inject the script
+    if (document.readyState === "complete") {
+        perfTime();
+    } else {
+        window.addEventListener("load", perfTime);
+    }
 }
